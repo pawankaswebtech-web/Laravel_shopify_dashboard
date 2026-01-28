3ef8ca04029f0cc32dba30539d010d110b188a4b
chandamin <chandamin084@gmail.com>
2026-01-28 17:24:44 +0530

    validator

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use OpenApi\Annotations as OA;
 use Carbon\Carbon;
 
