216392ca7b0f577ba98573882dbeba9157d34034
chandamin <chandamin084@gmail.com>
2026-02-23 12:51:22 +0530

    sdfsdf

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,12 @@ Route::middleware(['verify.shopify'])->group(function () {
     Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
     Route::get('/api/orders', [OrderController::class, 'fetchOrders'])->name('orders.fetch');
     Route::post('/orders/{id}/update-status', [OrderController::class, 'updateStatus'])->name('orders.update');
-  Route::get('/orders/{id}/resend-data/{token}', [OrderController::class, 'resendOrderData'])->name('orders.resend-data.public');
+  
+
 });
  
  Route::get('/orders/{id}/download-json', [OrderController::class, 'downloadJson'])->name('orders.download.json');
-
+Route::get('/orders/{id}/resend-data', [OrderController::class, 'resendOrderData'])->name('orders.resend-data');
 Route::get('/order-details', [OrderController::class, 'orderDetail'])->name('orders.orderdetails');
 Route::get('/order-details-view/{userId}', [OrderController::class, 'orderDetailView'])->name('orders.detailview');
 Route::get('/order-manage-status/{shopOrderId}/status', [OrderController::class, 'showOrderStatusForm'])->name('orders.status');
