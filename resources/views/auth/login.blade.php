2973d543b5ab1ad83d259b6019a649bc3d60d6bc
chandamin <chandamin084@gmail.com>
2026-02-23 18:43:38 +0530

    chg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -99,5 +99,6 @@ document.getElementById("togglePassword").addEventListener("click", function () 
     }
 });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
